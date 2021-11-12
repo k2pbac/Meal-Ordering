79b496e18d0c9409a1f9e1acd7296de9f1cedf4c
k2pbac <krisbachan123@hotmail.com>
2021-11-12 15:42:53 -0500

    add css and classes to button component

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css";
 
-import classes from "./HeaderCardButton.module.css";
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
 
-const HeaderCardButton = (props) => {};
-
-export default HeaderCardButton;
+export default HeaderCartButton;
