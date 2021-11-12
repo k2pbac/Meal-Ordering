855e0304f1330c4808fadc01c2ed40da34bd30ff
k2pbac <krisbachan123@hotmail.com>
2021-11-12 09:18:22 -0500

    add fresh files to repo

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <>
+      <header>
+        <h1>RealMeals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </>
+  );
+};
+
+export default Header;
